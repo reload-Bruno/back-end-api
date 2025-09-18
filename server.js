aad01ad4abb74b5e4393af44bce883722d8d00cf
reload-Bruno <blfp@aluno.ifnmg.edu.br>
2025-09-18 19:00:14 +0000

    .

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+app.get("/",async (req, res) => {        // Cria endpoint na rota da raiz do projeto
+  const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
   console.log("Rota GET / solicitada");
   res.json({
 		message: "API para viver e diferente de ta vivo ",      // Substitua pelo conteúdo da sua API
     author: "Bruno Luan Ferreira Pardinho",    // Substitua pelo seu nome
+    statusBD: dbStatus   // Acrescente esta linha
+
   });
 });
 
