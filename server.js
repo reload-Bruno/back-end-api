fc442743b4e5de0d98b9ac5334886acdc71eb575
reload-Bruno <blfp@aluno.ifnmg.edu.br>
2025-09-23 19:35:26 +0000

    .

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-import pkg from "pg";
+//server.js
+let pool = null; // Variável para armazenar o pool de conexões com o banco de dadosimport pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config();         // Carrega e processa o arquivo .env
 const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
@@ -6,20 +7,24 @@ import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/",async (req, res) => {        // Cria endpoint na rota da raiz do projeto
-  const db = new Pool({  
-  connectionString: process.env.URL_BD,
-});
-app.get("/questoes", (req, res)=> {
+
+//server.js
+// Função para obter uma conexão com o banco de dados
+function conectarBD() {
+  if (!pool) {
+    pool = new Pool({
+      connectionString: process.env.URL_BD,
+    });
+  }
+  return pool;
+}
+app.get("/questoes", async (req, res) => {
   const { Pool } = pkg; // Obtém o construtor Pool do pacote pg para gerenciar conexões com o banco de dados PostgreSQL
 
-const db = new Pool({
-  // Cria uma nova instância do Pool para gerenciar conexões com o banco de dados
-  connectionString: process.env.URL_BD, // Usa a variável de ambiente do arquivo .env DATABASE_URL para a string de conexão
-  
+//server.js
+const db = conectarBD(); // Cria uma nova instância do Pool para gerenciar conexões com o banco de dados
 
-  //server.js
-try {
+  try {
     const resultado = await db.query("SELECT * FROM questoes"); // Executa uma consulta SQL para selecionar todas as questões
     const dados = resultado.rows; // Obtém as linhas retornadas pela consulta
     res.json(dados); // Retorna o resultado da consulta como JSON
@@ -30,20 +35,24 @@ try {
       mensagem: "Não foi possível buscar as questões",
     });
   }
+
+
 });
 
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
 
-})
+//server.js
+const db = conectarBD(); // Cria uma nova instância do Pool para gerenciar conexões com o banco de dados
 
-let dbStatus = "ok";
-try {
-  await db.query("SELECT 1");
-} catch (e) {
-  dbStatus = e.message;
-}
+  let dbStatus = "ok";
+  try {
+    await db.query("SELECT 1");
+  } catch (e) {
+    dbStatus = e.message;
+  }
   console.log("Rota GET / solicitada");
   res.json({
-		message: "API para viver e diferente de ta vivo ",      // Substitua pelo conteúdo da sua API
+    message: "API para viver e diferente de ta vivo ",      // Substitua pelo conteúdo da sua API
     author: "Bruno Luan Ferreira Pardinho",    // Substitua pelo seu nome
     statusBD: dbStatus   // Acrescente esta linha
 
